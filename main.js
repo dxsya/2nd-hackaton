1652b51818a6f07f3eb36e4b059e9aac9cca7414
dxsya <dxgdxd@gmail.com>
2022-11-07 14:29:40 +0600

    photo add function

--- main.js
+++ main.js
@@ -41,4 +41,58 @@ async function addNewHighlight() {
 
 //TODO RENDER PHOTOS
 
-let addPhotoBtn = document.querySelector('.add-photo');
+let showModalAdd = document.querySelector('.create');
+let photoDialog = document.querySelector('#photo-add');
+let addPhoto = document.querySelector('#photoAdd');
+console.log(addPhoto);
+
+// add photo inputs
+let likeInp = document.querySelector('#likes');
+let commentInp = document.querySelector('#comments');
+let viewsInp = document.querySelector('#views');
+let photoInp = document.querySelector('#photo');
+
+let cancelButton2 = document.getElementById('cancel2');
+
+let photosList = document.querySelector('.photos');
+
+cancelButton2.addEventListener('click', function () {
+    photoDialog.close();
+});
+
+showModalAdd.addEventListener('click', function () {
+    photoDialog.showModal();
+});
+
+addPhoto.addEventListener('click', async function () {
+    let newPhoto = {
+        photo: photoInp.value,
+        likes: likeInp.value,
+        comments: commentInp.value,
+        views: viewsInp.value,
+    };
+    await fetch(APIphotos, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(newPhoto),
+    });
+    photoInp.value = '';
+    likeInp.value = '';
+    commentInp.value = '';
+    viewsInp.value = '';
+    render();
+});
+
+async function render() {
+    let photos = await fetch(APIphotos).then((res) => res.json());
+    console.log(photos);
+    photosList.innerHTML = '';
+    photos.forEach((element) => {
+        photosList.innerHTML += `<div class="photo-item" id='${element.id}'><div><img src=${element.photo}></div>
+        <div><p>${element.likes}</p><p>${element.comments}</p><p>${element.views}</p></div></div>`;
+    });
+}
+
+render();
